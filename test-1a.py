75a308d910d828bd8c368b3bd8aae8bb47d07646
site-adm2000 <site-adm2000@yandex.ru>
2022-08-22 21:18:20 +0300

    555

--- test-1a.py
+++ test-1a.py
@@ -3,6 +3,10 @@ print(Hello World Git)
 "vbkjhvjvhmfcjhgvk,"
 "drhrtjrtj"
 print('test-1a')
+<<<<<<< HEAD
 'jhilkhkljo;'
 '1111111111'
 '26554697-09=-0\\='
+=======
+'jhilkhkljo;'
+>>>>>>> parent of a9db3f3 (Update test-1a.py)
